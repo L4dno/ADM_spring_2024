ff3db4a461349613e5c48ae941b3e79a7a82fc21
Ladno <xmeloff12326@gmail.com>
2024-03-01 00:38:05 +0300

    done

--- lab2_comb/misha.py
+++ lab2_comb/misha.py
@@ -1,13 +1,50 @@
+# функция считает факториал числа
 def fac(n):
     if n<2:
         return 1
     return n * fac(n-1)
 
+# функция для перестановок без повторений
 def perm(n):
     return fac(n)
 
+# вычисление перестановок с повторениями
+# где a - массив длины n с количеством
+# разных элементов
 def perm_reps(n, a):
     tmp = 1
     for item in a:
         tmp *= fac(item)
-    return fac(n)/tmp
+    return int(fac(n)/tmp)
+
+
+# функция, реализующая задачу на перестановки без повторов
+def task_on_perm():
+    # в учебнике при n = 8 ответ 40320
+    condition = "Сколькими способами можно расположить на шахматной доске n ладей, \
+                    чтобы они «не били» друг друга?"
+    print(condition)
+    print("введите n")
+    n = int(input())
+    print("ответ: " + str(perm(n)))
+
+# функция, реализующая задачу на перестановки с повторениями
+def task_on_perm_reps():
+    # в учебнике слово = "уссури" и ответ 180
+    condition = "Сколько существует различных перестановок из \
+                    букв введенного слова?"
+    print(condition)
+    print("введите слово")
+    word = input()
+    letters = dict()
+    for symb in word:
+        letters[symb] = word.count(symb)
+    a= []
+    for letter in letters:
+        a.append(letters[letter])
+    print("ответ: " + str(perm_reps(len(word), a)))
+    
+#task_on_perm()
+task_on_perm_reps()
+
+
