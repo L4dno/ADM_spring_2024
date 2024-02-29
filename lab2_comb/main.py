ded64223b920b0cc2a6722e1581d1397d5ca6b4e
knyazev_a_p <knyazev_artem@icloud.com>
2024-03-01 00:52:47 +0300

    add func

--- lab2_comb/main.py
+++ lab2_comb/main.py
@@ -1,21 +1,17 @@
 #Лабороторная работа №2: Князев Артём, Хапков Михаил, Позоян Рафаэль
 def fact(a): # функция факториала
-    if a == 0:
+    if a == 0 or a == 1:
         return 1
-    elif a == 1:
-        return 1
-    elif a == 2:
-        return 2
     else:
-        return a*fact(a-1)
+        return a * fact(a - 1)
 
-def combinations_rep(n:int, k:int): # сочетание с повторениями (n+k-1)!/k!*(n-1)!
+def combinations_with_repetitions(n:int, k:int): # сочетание с повторениями (n+k-1)!/k!*(n-1)!
     otv = fact(n+k-1)/(fact(k)*fact(n-1))
-    print("Число равновозможных заказов равно:",int(otv))
-    return 0
+    print("Число равновозможных заказов равно: ",int(otv), '\n')
 
-def combination(): # сочетание без повторений n!/(n-x)!
-    pass 
+def combination_without_repetitions(n:int, k:int): # сочетание без повторений n!/(n-x)!
+    otv = fact(n)/(fact(n-k)*fact(k))
+    print("количество способов: ", int(otv), '\n')
 
 def main():
     print("Выберите тему:")
@@ -35,13 +31,16 @@ def main():
         if choose == 1:
             pass
         elif choose == 5:
-            print("В кофейне представлено n видов напитков, молодой человек просит дать ему k любых напитков")
-            n = int(input('введите n:'))
-            k = int(input('введите k'))
-            combinations_rep(n, k)
+            print("n ребят собрали в саду k яблока. Сколькими способами они могут их разделить между собой?") # в задаче из учебника n = 3, k=63, C=2080
+            n = int(input('введите n: '))
+            k = int(input('введите k: '))
+            combinations_with_repetitions(n, k)
         elif choose == 6: 
-            pass
-        
+            print("У вас есть n различных книг, и вы хотите выбрать k книги для чтения. Сколькими способами вы можете выбрать эти книги?") # в задаче n=5, k=3, C=10
+            n = int(input('введите n: '))
+            k = int(input('введите k: '))
+            combination_without_repetitions(n, k)
+
         print("Выберите тему:")
         print("1 - Правило суммы")
         print("2 - Правило произведения")
